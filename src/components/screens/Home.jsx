a3e3d79f7c931f3dc927d5cf62399b6b9557d7db
Wellerson <wellerson.coffee@gmail.com>
2019-09-06 20:17:08 -0300

    add redux thunk

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -2,32 +2,25 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { clickButton } from "../../actions";
-import Title from "../Title/Title";
 import Header from "../Header/Header";
+import List from "../List/List";
+
 
 const Home = props => {
-  const { newValue, clickButton } = props;
-  const [inputValue, setInputValue] = useState("");
-
-  const inputChange = event => {
-    setInputValue(event.target.value);
-  };
 
   return (
     <>
       <Header />
+      <List/>
     </>
   );
 };
 
-const mapStateToProps = store => ({
-  newValue: store.clickState.newValue
-});
+// const mapStateToProps = store => ({
+//   newValue: store.clickState.newValue
+// });
 
-const mapDispatchToProps = dispatch => ({
-  clickButton: bindActionCreators(clickButton, dispatch)
-});
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+// const mapDispatchToProps = dispatch => ({
+//   clickButton: bindActionCreators(clickButton, dispatch)
+// });
+export default Home;
